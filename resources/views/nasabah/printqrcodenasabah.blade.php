c6063666e9b10ce5f8a29032b86361c12077d9fb
Sultan Maula <sultanmaulachamzah@gmail.com>
2020-09-04 14:00:42 +0700

    add tombol kembali di print code qr

--- resources/views/nasabah/printqrcodenasabah.blade.php
+++ resources/views/nasabah/printqrcodenasabah.blade.php
@@ -2,6 +2,7 @@
     <div class="form-group col-md-6">
         <div>{!! QrCode::size('200')->generate($nasabah[0]->id); !!}</div>
     	<h4>{{ $nasabah[0]->nama }}</h4>
+    	<a href="/nasabah" class="btn btn-danger" type="button">Kembali</a>
     </div>
 </div>
 <script type="text/javascript">
